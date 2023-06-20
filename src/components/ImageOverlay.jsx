b25ff266b31bfca029a37c91a588cafd4a716e8a
krishbale <sushil.pokharel.7528@gmail.com>
2023-06-20 23:33:31 +0545

    imageadded

--- src/components/ImageOverlay.jsx
+++ src/components/ImageOverlay.jsx
@@ -2,7 +2,7 @@
 export const ImageOverlay = () => {
   return (
     <div>
-      <div className="bg-[url('src/assets/Imageoverlay.png')] h-[348px] mt-2  bg-cover bg-no-repeat ">
+      <div className="bg-image-overlay h-[348px] mt-2  bg-cover bg-no-repeat ">
         <div className="bg-black bg-opacity-30  text-white h-full">
           <div className="m400 h-full flex flex-col justify-center    ">
           <h2 className="lg:text-4xl md:text-3xl sm:text-2xl text-xl font-bold p-4 m-4 " >Buy 2 and get 20% OFF</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('src/assets/bg.png')",
+        'image-overlay': "url('src/assets/Imageoverlay.png')",
+      }
+    },
   },
   plugins: [],
 }
